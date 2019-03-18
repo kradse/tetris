006ee4ad7d912ec693a394e3bf76555f228e7930
Christian M. Schachner <kradse@gmail.com>
2019-03-18 14:47:31 +0100

    first commit

---
+++ init.js
@@ -0,0 +1,15 @@
+import { test } from "./src/main.js"
+
+// const canvas = document.getElementById('screen')
+// canvas.width = 1367
+// canvas.height = 768
+// canvas.style = "border:1px solid black;"
+
+// const fps = 16
+// const interval = 1000 / fps
+
+const init = () => {
+	test()
+}
+
+init()
